a9008b95af8b9ae557684e708c80a766b847f86a
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2009-11-04 09:05:57 +0000

    Import of TOKUHIROM/CGI-Emulate-PSGI-0.02 from CPAN.
    gitpan-cpan-distribution: CGI-Emulate-PSGI
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TOKUHIROM/CGI-Emulate-PSGI-0.02.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/CGI/Emulate/PSGI.pm
+++ lib/CGI/Emulate/PSGI.pm
@@ -4,9 +4,9 @@ use warnings;
 use POSIX 'SEEK_SET';
 use HTTP::Response;
 use IO::File;
-use 5.00800;
+use 5.008;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub handler {
     my ($class, $code, ) = @_;
